769e72d23e410e74ca6a9ae969c03c4d1bfb730a
Bruno Lodeiro <lodeirobruno@gmail.com>
2025-10-30 10:45:34 -0300

    feat: agrego passwordencoder, depencias y aplico en UsserAccount

--- src/main/java/com/poo/proyecto/ProyectoApplication.java
+++ src/main/java/com/poo/proyecto/ProyectoApplication.java
@@ -1,15 +1,26 @@
 package com.poo.proyecto;
 
+import com.poo.proyecto.util.PasswordEncoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableJpaAuditing
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ProyectoApplication.class, args);
+
+
+	}
+
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new PasswordEncoder();
 	}
 
 }
